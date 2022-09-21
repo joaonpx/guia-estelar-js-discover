e3d22ba9f00a3a9aa30b2993af151bef05732ca5
Npx <cheyprivado@gmail.com>
2022-09-21 17:10:49 -0300

    :hammer: Atualizando declaration assignment

--- variaveis/declaration-assignment.js
+++ variaveis/declaration-assignment.js
@@ -6,3 +6,11 @@ name = 'João'
 
 // que tipo de dado foi colocado com a função typeof
 console.log(typeof name)
+
+// agripando declarations
+let age, isHuman
+
+age = 18
+isHuman = true
+
+console.log(name, age, isHuman)
