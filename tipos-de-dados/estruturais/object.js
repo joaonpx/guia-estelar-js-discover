ad1a3fb7c6adb17b3140da94aac8e96af3211839
Npx <cheyprivado@gmail.com>
2022-09-21 17:39:58 -0300

    :hammer: Atualizando object

--- tipos-de-dados/estruturais/object.js
+++ tipos-de-dados/estruturais/object.js
@@ -1,9 +1,18 @@
 // object
 
-console.log({
-  name: 'João',
+// console.log({
+//   name: 'João',
+//   age: 18,
+//   andar: function () {
+//     console.log('andando')
+//   }
+// })
+
+let user = {
+  name: 'John',
   age: 18,
-  andar: function () {
-    console.log('andando')
-  }
-})
+  isAdmin: true
+}
+
+// acessando as propriedades de um objeto
+console.log(user.name)
