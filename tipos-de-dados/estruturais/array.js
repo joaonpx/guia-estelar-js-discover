c151f1c502d5d813f04c466225bcf641808f1689
Npx <cheyprivado@gmail.com>
2022-09-21 17:40:13 -0300

    :hammer: Atualizando array

--- tipos-de-dados/estruturais/array.js
+++ tipos-de-dados/estruturais/array.js
@@ -1,3 +1,10 @@
 // array
 
-console.log(['João', 18])
+// console.log(['João', 18])
+let animals = ['Lion', 'Monkey', 'Cat']
+
+// // acessando valores dentro do array
+// console.log(animals[2])
+
+// descobrindo o número de posições dentro do array
+console.log(animals.length)
