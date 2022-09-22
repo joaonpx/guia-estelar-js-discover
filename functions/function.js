295b77b6a15dc1bc0bbee6121d7a522b939a1c30
Npx <cheyprivado@gmail.com>
2022-09-22 17:47:44 -0300

    :hammer: Adicionando retornando valores

---
+++ functions/function.js
@@ -0,0 +1,12 @@
+// retornando valores
+
+function sum(number1, number2) {
+  return number1 + number2
+}
+
+let number1 = 2
+let number2 = 3
+
+console.log(`o número primeiro é: ${number1}`)
+console.log(`o número segundo é: ${number2}`)
+console.log(`o resultado é ${sum(number1, number2)}`)
