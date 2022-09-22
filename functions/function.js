6a7fe8eca489137a208ed2b0f1fb81c8c317b478
Npx <cheyprivado@gmail.com>
2022-09-22 17:54:56 -0300

    :hammer: Adicionando function scope

--- functions/function.js
+++ functions/function.js
@@ -1,12 +1,23 @@
 // retornando valores
 
-function sum(number1, number2) {
-  return number1 + number2
+// function sum(number1, number2) {
+//   return number1 + number2
+// }
+
+// let number1 = 2
+// let number2 = 3
+
+// console.log(`o número primeiro é: ${number1}`)
+// console.log(`o número segundo é: ${number2}`)
+// console.log(`o resultado é ${sum(number1, number2)}`)
+
+// function scope
+let subject = 'study'
+
+function pinto(subject) {
+  subject = 'read'
+  return subject
 }
 
-let number1 = 2
-let number2 = 3
-
-console.log(`o número primeiro é: ${number1}`)
-console.log(`o número segundo é: ${number2}`)
-console.log(`o resultado é ${sum(number1, number2)}`)
+console.log(pinto(subject))
+console.log(subject)
