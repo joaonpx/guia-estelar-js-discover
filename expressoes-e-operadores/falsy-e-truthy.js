af061b969a14f37d7755c16f114514e3a0e09d84
Npx <cheyprivado@gmail.com>
2022-09-25 20:19:55 -0300

    :hammer: Adicionando falsy e truthy

---
+++ expressoes-e-operadores/falsy-e-truthy.js
@@ -0,0 +1,28 @@
+/*
+	Todos os valores abaixo seriam representados como false em um boolean.
+		false
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+
+console.log(NaN ? 'verdadeiro' : 'falso')
+
+/*
+	Todos os valores abaixo seriam representados como false em um boolean.
+		true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+
+console.log(Infinity ? 'verdadeiro' : 'falso')
