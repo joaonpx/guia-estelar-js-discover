884db96c3e0d98d6d2c434ca39836d88662aaf63
Npx <cheyprivado@gmail.com>
2022-09-25 20:09:32 -0300

    :hammer: Adicionando operadores de comparação

---
+++ expressoes-e-operadores/operadores-de-comparacao.js
@@ -0,0 +1,26 @@
+let one = 1
+let two = 2
+
+// igual a: ==
+// console.log(one == two)
+
+// diferente de: !=
+// console.log(one != two)
+
+// estritamente igual: ===
+// console.log(one === two)
+
+// estritamente diferente: !==
+// console.log(one !== two)
+
+// maior que: >
+// console.log(one > two)
+
+// menor que: <
+// console.log(one < two)
+
+// maior ou igual a: >=
+// console.log(one >= two)
+
+// menor ou igual a: <=
+// console.log(one <= two)
