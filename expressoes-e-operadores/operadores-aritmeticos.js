21b18ca8fdb0b2d876bea13f5fbd7b3703a3ca2b
Npx <cheyprivado@gmail.com>
2022-09-25 19:52:17 -0300

    :hammer: Adicionando operadores aritmeticos

---
+++ expressoes-e-operadores/operadores-aritmeticos.js
@@ -0,0 +1,32 @@
+// let number = 6
+
+// mutiplicacão
+// console.log(number * 2)
+
+// divisão
+// console.log(number / 2)
+
+// soma
+// console.log(number + 4)
+
+// subtração
+// console.log(number - 4)
+
+/* resto da divisão: sinal % */
+// let remainder
+// remainder = 11 % 9
+// console.log(remainder)
+
+/* incremento: sinal ++ */
+// let increment = 0
+
+// console.log(++increment)
+// console.log(increment)
+
+/* decremento: sinal -- */
+// let decrement = 0
+// decrement--
+// console.log(decrement)
+
+/* exponencial: sinal ** */
+// console.log(2 ** 3)
