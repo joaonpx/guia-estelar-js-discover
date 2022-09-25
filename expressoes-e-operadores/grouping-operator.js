2f1f8cf442fd5b2f8e5642be08522c08fcee1fbf
Npx <cheyprivado@gmail.com>
2022-09-25 20:05:06 -0300

    :hammer: Adicionando grouping operator

---
+++ expressoes-e-operadores/grouping-operator.js
@@ -0,0 +1,7 @@
+// let total = 2 + 3 * 5
+// console.log(total)
+//com essa precedência, nosso resultado é 17.
+
+let total = (2 + 3) * 5
+console.log(total)
+// o cálculo mudou a precedência, fazendo nosso resultado tornar-se 25.
