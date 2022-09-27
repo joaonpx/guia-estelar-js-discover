1955db4277948b06a83e870f66b38f83c110a36a
Npx <cheyprivado@gmail.com>
2022-09-27 19:31:24 -0300

    :hammer: Adicionando fluxo de caixa familiar

---
+++ exercicios/fluxo-de-caixa-familiar.js
@@ -0,0 +1,21 @@
+/* 
+  Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+    * receitas: [] 
+    * despesas: []
+  Agora, crie uma função que irá calcular o total de receitas e 
+  despesas e irá mostrar uma mensagem se a família está com 
+  saldo positivo ou negativo, seguido do valor do saldo.
+*/
+
+let cashier = {
+  revenues: [100, 250, 1890],
+  expenses: [28, 16, 89.5, 32]
+}
+
+function sum(array) {
+  let total
+
+  return total
+}
+
+console.log(sum(cashier.revenues))
