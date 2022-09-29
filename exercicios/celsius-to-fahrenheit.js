5d80e701c6a5805c60fc6fad2a870549c0dee5c2
Npx <cheyprivado@gmail.com>
2022-09-29 18:01:03 -0300

    :hammer: Atualizando celsius to fahrenheit

--- exercicios/celsius-to-fahrenheit.js
+++ exercicios/celsius-to-fahrenheit.js
@@ -4,3 +4,18 @@
       C = (F - 32) * 5/9 
       F = C * 9/5 + 32
 */
+
+function celsiusToFahrenheit(c) {
+  let result = (c * 9) / 5 + 32
+
+  return result
+}
+
+function fahrenheitToCelsius(f) {
+  let result = ((f - 32) * 5) / 9
+
+  return result
+}
+
+console.log(celsiusToFahrenheit(0))
+console.log(fahrenheitToCelsius(32))
